923659ce701ee1aad2e84788ee2185374c22e6ff
amarillo-sebas <amarillo.sebas@gmail.com>
2018-05-15 12:55:16 -0500

    Objetivos
    -la versión anterior también tenía el minimapa con vida y munición
    -objetivo que hace snap (buggeado - interactúa con hitbox)

---
+++ Assets/Scripts/Interactables/SelfDestruct_Objectives/SelfDestructObjectiveZonePlacer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructObjectiveZonePlacer : MonoBehaviour {
+	public GameObject objetiveZonePrefab;
+
+	void Start () {
+		GameObject oz = Instantiate(objetiveZonePrefab, transform.position, transform.rotation);
+		oz.transform.parent = transform;
+	}
+}
---
+++ Assets/Scripts/Interactables/SelfDestruct_Objectives/SelfDestruct_Objective_A.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct_Objective_A : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
