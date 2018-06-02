d2ea3dc11e95f2fd007e12ed94eee36ca39aa68e
amarillo-sebas <amarillo.sebas@gmail.com>
2018-06-01 22:30:53 -0500

    Corrected SceneViewGizmos script
    - para que el juego pueda construirse

--- Assets/Scripts/Utils/SceneViewGizmos.cs
+++ Assets/Scripts/Utils/SceneViewGizmos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if (UNITY_EDITOR) 
+using System;
 using System.Collections;
 using System.Reflection;
 using UnityEditor;
@@ -45,3 +46,4 @@ public class SceneViewGizmos {
 		}
 	}
 }
+#endif
